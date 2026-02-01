3e60bebee169657e0a63ccb89cfc1ebd9ea70426
Sonya <94686155+slooonya@users.noreply.github.com>
2026-02-01 21:42:39 +0800

    style: add labels to weekly images

--- client/src/pages/gallery/WeeklyImages.tsx
+++ client/src/pages/gallery/WeeklyImages.tsx
@@ -2,13 +2,16 @@ import TestImage from '../../assets/images/test-image.png';
 import './WeeklyImages.css';
 
 export function WeeklyImages() {
+  const images = [TestImage, TestImage, TestImage, TestImage, TestImage];
+
   return (
     <div className="weekly-images">
-      <img src={TestImage} alt="" />
-      <img src={TestImage} alt="" />
-      <img src={TestImage} alt="" />
-      <img src={TestImage} alt="" />
-      <img src={TestImage} alt="" />
+      {images.map((img, i) => (
+        <div key={i} className="weekly-image">
+          <img src={img} />
+          <span>Week {i + 1}</span>
+        </div>
+      ))}
     </div>
   );
 }
